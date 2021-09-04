ec95eb40872156d2200e9045abe0bb46a015826e
천수빈 <suebin710@gmail.com>
2021-09-04 21:11:06 +0900

    add model

--- database/models.py
+++ database/models.py
@@ -1,5 +1,13 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, LargeBinary, BINARY
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
+class Music(Base):
+    __tablename__ = "music"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
+    title = Column(String, nullable=False)
+    info = Column(String, nullable=False)
+    image_link = Column(String)
+    wavFile = Column(LargeBinary)
